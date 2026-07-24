59c25efaf547ea4e9b68fd8655b2855aafec628b
Osakpolor Obaseki <obasekiosa@gmail.com>
2026-07-24 10:23:08 +0100

    release: add v0.1.0 checksums and notes

---
+++ checksum-Elixir.OgEx.Native.exs
@@ -0,0 +1,9 @@
+%{
+  "libog_ex_native-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:623a0c4c6b649e71dc45f9c90bfbc8d26942c3f5a650831b65a7dfb91ed34094",
+  "libog_ex_native-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:6408425f9bf764f30c3aaae8bc3f1f43628c786fccaf6f634c229501b5ed5015",
+  "libog_ex_native-v0.1.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:ecd90edfeeab6b3eca43def254b6eb61401da00ebcb9a71517fac1e274ca7a61",
+  "libog_ex_native-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:5f0857995be85937cf98f0be1f3a8d0a8327c82fb7513938efd305fd488acb4b",
+  "libog_ex_native-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:05320730d1b38c8c949695b053cbfb59188ad9543af7bd54b2297c2511e3ed47",
+  "libog_ex_native-v0.1.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:c5b474af2bc2667271584581d117782f662bfac21c9422a2b2ea7df06504e6ed",
+  "og_ex_native-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:5b292b8fc01f92b03775e75d8b745c0b7844264ddcec485daef9ac136ec891a7",
+}
